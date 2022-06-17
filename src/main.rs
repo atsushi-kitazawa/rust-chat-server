40cc7bdaec5afc2d6f14559ae6c021966b80c62b
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-06-17 09:39:00 +0900

    simple tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let result = TcpListener::bind("127.0.0.1:8888");
+
+    match result {
+        Result::Ok(listner) => {
+            accept(listner)
+        },
+        Result::Err(_e) => {println!("NG: bind()")},
+    }
+}
+
+fn accept(listner: TcpListener) {
+    let stream = listner.accept();
+    match stream {
+        Result::Ok((mut _stream, _)) => {println!("OK")},
+        Result::Err(_e) => {println!("NG: accept()")},
+    }
 }
