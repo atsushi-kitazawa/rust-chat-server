53d6fc651b9eb8ff27bd352f21ab02504a86701b
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-06-17 10:24:35 +0900

    Multi-threaded request processing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
     str,
 };
 
@@ -11,7 +12,9 @@ fn main() {
         Result::Ok(listner) => {
             for stream in listner.incoming() {
                 let stream = stream.unwrap();
-                handle_connection(stream)
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
             }
         }
         Result::Err(_e) => {
@@ -21,11 +24,14 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 512];
-    stream.read(&mut buf).unwrap();
-    println!("request {}", String::from_utf8_lossy(&buf[..]));
+    let client_address = stream.peer_addr().unwrap();
+    loop {
+        let mut buf = [0; 512];
+        stream.read(&mut buf).unwrap();
+        println!("request({}) {}", client_address, String::from_utf8_lossy(&buf[..]));
 
-    let reply = String::from("hello ") + str::from_utf8(&buf).unwrap();
-    stream.write(reply.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let reply = String::from("hello ") + str::from_utf8(&buf).unwrap();
+        stream.write(reply.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
