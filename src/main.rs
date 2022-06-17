c8f9cf1c1dcdd57259d5cafbe2f37cba95a44d7e
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-06-17 10:12:30 +0900

    add request, reply.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    str,
+};
 
 fn main() {
     let result = TcpListener::bind("127.0.0.1:8888");
 
     match result {
         Result::Ok(listner) => {
-            accept(listner)
-        },
-        Result::Err(_e) => {println!("NG: bind()")},
+            for stream in listner.incoming() {
+                let stream = stream.unwrap();
+                handle_connection(stream)
+            }
+        }
+        Result::Err(_e) => {
+            println!("NG: bind()")
+        }
     }
 }
 
-fn accept(listner: TcpListener) {
-    let stream = listner.accept();
-    match stream {
-        Result::Ok((mut _stream, _)) => {println!("OK")},
-        Result::Err(_e) => {println!("NG: accept()")},
-    }
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    stream.read(&mut buf).unwrap();
+    println!("request {}", String::from_utf8_lossy(&buf[..]));
+
+    let reply = String::from("hello ") + str::from_utf8(&buf).unwrap();
+    stream.write(reply.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
